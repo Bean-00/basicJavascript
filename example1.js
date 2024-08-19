ac4bb82e042f85bb733c99238797d6a2daf6212a
bean <dbher.0010@gmail.com>
2024-08-19 23:46:46 +0900

    파일 생성

---
+++ example1.js
@@ -0,0 +1,15 @@
+console.log('This is "String"');
+
+if ('가방' > '백')
+    console.log("가방이 크다");
+else
+    console.log("백이 크다");
+
+
+if (30>20>10) {
+    console.log("이건 안 나오지롱");
+} else {
+    console.log("1>10이 돼서 False가 나온다.");
+}
+
+console.log(typeof(typeof 52)); //string
